5fdb998ac2203f92d60ed10acee30942b3343c6e
sandron_m <mickael@kod-it.be>
2017-03-05 20:42:20 +0100

    Step3

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Step 1 - Initial issue
+# Step 3 - No problem with the old version of Spark 2.5.1 (see pom.xml)
 1. Run Main.class
 2. Visit [http://localhost:8080](http://localhost:8080) => "Hello from server1"
-3. Visit [http://localhost:8080/image.jpg](http://localhost:8080/image.jpg) => 404 **:(**
+3. Visit [http://localhost:8080/image.jpg](http://localhost:8080/image.jpg) => 200 **:)**
 4. Visit [http://localhost:8181](http://localhost:8181) => "Hello from server2"
 5. Visit [http://localhost:8181/image.jpg](http://localhost:8181/image.jpg) => 200 **:)**
-6. Change branch => git checkout --track origin/step2
--- src/main/java/be/kodit/spark/Main.java
+++ src/main/java/be/kodit/spark/Main.java
@@ -11,13 +11,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        # Step 1 - Initial issue
+//        # Step 3 - No problem with the old version of Spark 2.5.1 (see pom.xml)
 //        1. Run Main.class
 //        2. Visit [http://localhost:8080](http://localhost:8080) => "Hello from server1"
-//        3. Visit [http://localhost:8080/image.jpg](http://localhost:8080/image.jpg) => 404 **:(**
+//        3. Visit [http://localhost:8080/image.jpg](http://localhost:8080/image.jpg) => 200 **:)**
 //        4. Visit [http://localhost:8181](http://localhost:8181) => "Hello from server2"
 //        5. Visit [http://localhost:8181/image.jpg](http://localhost:8181/image.jpg) => 200 **:)**
-//        6. Change branch => git checkout origin/step2
 
         Service server1 = Service.ignite().port(8080);
         Service server2 = Service.ignite().port(8181);

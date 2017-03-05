0685447f98102f2c6f3fd59b0ad6ba1760126832
sandron_m <mickael@kod-it.be>
2017-03-05 20:31:52 +0100

    Step2

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Step 1 - Initial issue
+# Step 2 - No problem with only 1 server
 1. Run Main.class
 2. Visit [http://localhost:8080](http://localhost:8080) => "Hello from server1"
-3. Visit [http://localhost:8080/image.jpg](http://localhost:8080/image.jpg) => 404 **:(**
-4. Visit [http://localhost:8181](http://localhost:8181) => "Hello from server2"
-5. Visit [http://localhost:8181/image.jpg](http://localhost:8181/image.jpg) => 200 **:)**
-6. Change branch => git checkout origin/step2
+3. Visit [http://localhost:8080/image.jpg](http://localhost:8080/image.jpg) => 200 **:)**
+6. Change branch => git checkout origin/step3
--- src/main/java/be/kodit/spark/Main.java
+++ src/main/java/be/kodit/spark/Main.java
@@ -11,21 +11,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        # Step 1 - Initial issue
+//        # Step 2 - No problem with only 1 server
 //        1. Run Main.class
 //        2. Visit [http://localhost:8080](http://localhost:8080) => "Hello from server1"
-//        3. Visit [http://localhost:8080/image.jpg](http://localhost:8080/image.jpg) => 404 **:(**
-//        4. Visit [http://localhost:8181](http://localhost:8181) => "Hello from server2"
-//        5. Visit [http://localhost:8181/image.jpg](http://localhost:8181/image.jpg) => 200 **:)**
-//        6. Change branch => git checkout origin/step2
+//        3. Visit [http://localhost:8080/image.jpg](http://localhost:8080/image.jpg) => 200 **:)**
+//        6. Change branch => git checkout origin/step3
 
         Service server1 = Service.ignite().port(8080);
-        Service server2 = Service.ignite().port(8181);
+//        Service server2 = Service.ignite().port(8181);
 
         server1.externalStaticFileLocation(Paths.get("folder1").toAbsolutePath().toString());
-        server2.externalStaticFileLocation(Paths.get("folder2").toAbsolutePath().toString());
+//        server2.externalStaticFileLocation(Paths.get("folder2").toAbsolutePath().toString());
 
         server1.get("/", (req, resp) -> "Hello from server1");
-        server2.get("/", (req, resp) -> "Hello from server2");
+//        server2.get("/", (req, resp) -> "Hello from server2");
     }
 }
